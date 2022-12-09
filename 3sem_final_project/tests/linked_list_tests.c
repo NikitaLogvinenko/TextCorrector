34185b500c71ac1055c5fe29b75af0802820693f
NikitaLogvinenko <nichnikich@gmail.com>
2022-12-09 08:15:31 +0700

    add function declarations

--- 3sem_final_project/tests/linked_list_tests.c
+++ 3sem_final_project/tests/linked_list_tests.c
@@ -3,14 +3,107 @@
 #include "linked_list_tests.h"
 #include <locale.h>
 #include <assert.h>
+#include <stdlib.h>
 
 // ¬ случае успешного выполнени€ тестов возвращаем 0
+
+static Linked_list* create_llist_for_test();
 
 int launch_all_tests()
 {
 	setlocale(LC_ALL, "ru");
 	create_destroy_test();
-	
+	append_test();
+	prepend_test();
+	concat_test();
+	copy_test();
+	find_test();
+	find_custom_test();
+	foreach_test();
+	get_test();
+	get_last_test();
+	length_test();
+	remove_test();
+	remove_all_equal_test();
+	remove_first_equal_test();
+
 	printf("¬се тесты успешно пройдены\n");
 	return 0;
+}
+
+int create_destroy_test()
+{
+	return 0;
+}
+
+int append_test()
+{
+	return 0;
+}
+
+int prepend_test()
+{
+	return 0;
+}
+
+int concat_test()
+{
+	return 0;
+}
+
+int copy_test()
+{
+	return 0;
+}
+
+int find_test()
+{
+	return 0;
+}
+
+int find_custom_test()
+{
+	return 0;
+}
+
+int foreach_test()
+{
+	return 0;
+}
+
+int get_test()
+{
+	return 0;
+}
+
+int get_last_test()
+{
+	return 0;
+}
+
+int length_test()
+{
+	return 0;
+}
+
+int remove_test()
+{
+	return 0;
+}
+
+int remove_all_equal_test()
+{
+	return 0;
+}
+
+int remove_first_equal_test()
+{
+	return 0;
+}
+
+static Linked_list* create_llist_for_test()
+{
+	Linked_list* list_for_test = (Linked_list*)malloc(sizeof(Linked_list));
+
+	return list_for_test;
 }
--- 3sem_final_project/tests/linked_list_tests.h
+++ 3sem_final_project/tests/linked_list_tests.h
@@ -3,5 +3,33 @@
 
 int launch_all_tests();
 
+int create_destroy_test();
+
+int append_test();
+
+int prepend_test();
+
+int concat_test();
+
+int copy_test();
+
+int find_test();
+
+int find_custom_test();
+
+int foreach_test();
+
+int get_test();
+
+int get_last_test();
+
+int length_test();
+
+int remove_test();
+
+int remove_all_equal_test();
+
+int remove_first_equal_test();
+
 #endif
 
