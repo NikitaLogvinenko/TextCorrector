2de490c1fbd576d4a9255cc88ef24ff4d345281c
NikitaLogvinenko <nichnikich@gmail.com>
2022-12-24 22:27:28 +0700

    Part with parameters is finished

--- 3sem_final_project/main.c
+++ 3sem_final_project/main.c
@@ -16,21 +16,25 @@ int main(int argc, char** argv)
 	SetConsoleOutputCP(1251);
 
 	printf("\n*********************************************************************************************************************\n");
-	printf("# Настройка конфигурации программы...\n\n");
+	if (argc > 1 && strcmp("help", argv[1]) != 0)
+		printf("# Настройка конфигурации программы...\n\n");
 	Pointer* cfg = set_cfg(argc, argv);  // в случае неудачной установки конфигурации программа завершится внутри set_cfg через exit с очисткой памяти
 	printf("\n# Конфигурация успешно настроена!\n\n");
 	int mode = *(int*)cfg[0];
 	switch (mode)
 	{
 	case TRAIN_NEW_MODE:
+		print_train_new_cfg(cfg);
 		printf("\n# Запуск обучения новой модели\n\n");
 		exit_code = train_new_model(cfg);
 		break;
 	case TRAIN_EXISTED_MODE:
+		print_train_existed_cfg(cfg);
 		printf("\n# Запуск обучения существующей модели\n\n");
 		exit_code = train_existed_model(cfg);
 		break;
 	case EDIT_MODE:
+		print_edit_cfg(cfg);
 		printf("\n# Запуск редактирования текста\n\n");
 		exit_code = edit_text(cfg);
 		break;
